dae65a200b158a92f2c58369bb64ac7a75dca5e8
Daniel Lozano <dlozanoc862@gmail.com>
2023-02-28 14:17:25 -0500

    Using useEffect and consuming the API

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import iconAddToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+
+    const [cart, setCart] = useState([]);
+
+    const handleCart = () => {
+        setCart([]);
+    }
+
     return (
         <div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,7 +18,7 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleCart}>
 					<img src={iconAddToCart} alt="" />
 				</figure>
 			</div>
